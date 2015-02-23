5735dc3eb3341eb7ec0cb644de2b69aea5e6c828
Wes Schlichter <wschlichter@pivotal.io>
2015-02-23 10:43:17 -0600

    Update example

--- src/main/java/com/springsource/tcruntime/embedded/bootexample/ExampleController.java
+++ src/main/java/com/springsource/tcruntime/embedded/bootexample/ExampleController.java
@@ -13,6 +13,13 @@ public class ExampleController {
   public String index() {
     Class klass = Tomcat.class;
     URL location = klass.getResource('/'+klass.getName().replace('.', '/')+".class");
-    return "org.apache.catalina.startup.Tomcat class was loaded from: " + location.toString();
+      StringBuilder sb = new StringBuilder();
+      sb.append("Greetings from spring-boot-starter-tcserver-example! \n");
+      sb.append("This example will show the jar which loads the ");
+      sb.append(Tomcat.class.toString());
+      sb.append(". Please verify you see the jar file (com.springsource.org.apache.tomcat.embed.core-<version>.jar)");
+      sb.append("on the next line\n");
+      sb.append(location.toString() + "\n");
+    return sb.toString();
   }
 }
