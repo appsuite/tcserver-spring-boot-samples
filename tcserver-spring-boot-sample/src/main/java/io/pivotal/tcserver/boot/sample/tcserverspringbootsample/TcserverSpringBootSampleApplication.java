416f553590e3c4d1d19be75b775f2dd39286850e
Wes Schlichter <wschlichter@pivotal.io>
2018-05-01 09:53:29 -0500

    Update for tc Server Spring Boot 4.0.1

---
+++ tcserver-spring-boot-sample/src/main/java/io/pivotal/tcserver/boot/sample/tcserverspringbootsample/TcserverSpringBootSampleApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.tcserver.boot.sample.tcserverspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcserverSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcserverSpringBootSampleApplication.class, args);
+	}
+}
